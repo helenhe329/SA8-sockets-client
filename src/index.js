cfc112c9dc7f306dfa5325a891a5f3b492a6bd39
helenhe329 <helenhe329@gmail.com>
2018-04-13 19:13:06 -0400

    deployed on surge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+import './style.scss';
+
 const $ = require('jquery');
 
-$('#main').html('Here we go!');
+let num = 0;
+window.setInterval((() => {
+  if (num === 1) {
+    $('#main').html(`You've been on this page for ${num} second`);
+  } else {
+    $('#main').html(`You've been on this page for ${num} seconds`);
+  }
+  num += 1;
+}), 1000);
